306ec3b0f50ce208b7e03358eb620f0730115ba0
Mohaiman <m4mohaiman@gmail.com>
2025-03-13 15:48:25 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-blue-500">Hello world Mohaiman</h1>
+    </div>
+  );
+}
